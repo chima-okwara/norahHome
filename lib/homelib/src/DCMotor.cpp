f985f88aeb7c88587edeeb57e721d94c2ce948e1
chima <cokwara82@gmail.com>
2023-04-02 10:12:21 +0100

    Added preliminary firmware for modules

---
+++ lib/homelib/src/DCMotor.cpp
@@ -0,0 +1,67 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//*FILE NAME:       home.h
+//*FILE DESC:       Implementation file for DCMotor.
+//*FILE VERSION:    0.1.1
+//*FILE AUTHOR:     Chimaroke Okwara
+//*LAST MODIFIED:   Saturday, 1 April, 2023 20:19
+//*LICENSE:         Academic Free License
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <Arduino.h>
+#include "home.h"
+#ifndef MRES
+#define MRES 16
+#endif
+DCMotor::DCMotor(pin_t mot1, pin_t mot2, pin_t en):_mot1(mot1), _mot2(mot2), _en(en), _dir(forward)
+{
+
+}
+
+bool DCMotor::begin()
+{
+    pinMode(_mot1, PWM);
+    pinMode(_mot2, PWM);
+    pinMode(_en, OUTPUT);
+    analogWriteResolution(MRES);
+    return(true);
+}
+
+const direction_t& DCMotor::move(const direction_t& dir, pin_t speed)
+{
+    switch(dir)
+    {
+        analogWriteResolution(MRES);
+        case forward:
+        {
+            digitalWrite(_en, HIGH);
+            analogWrite(_mot1, speed);
+            analogWrite(_mot2, 0);
+            _dir = forward;
+            break;
+        }
+
+        case backward:
+        {
+            digitalWrite(_en, HIGH);
+            analogWrite(_mot1, 0);
+            analogWrite(_mot2, speed);
+            _dir = backward;
+            break;
+        }
+    }
+
+    return (_dir);
+}
+
+
+void DCMotor::stop()
+{
+    digitalWrite(_en, 0);
+    analogWrite(_mot1, 0);
+    analogWrite(_mot2, 0);
+}
+
+
+const direction_t& DCMotor::getDir() const
+{
+    return(_dir);
+}
---
+++ lib/homelib/src/LEDDriver.cpp
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//*FILE NAME:       home.h
+//*FILE DESC:       Implementation file for LEDDriver.
+//*FILE VERSION:    0.1.1
+//*FILE AUTHOR:     Chimaroke Okwara
+//*LAST MODIFIED:   Saturday, 1 April, 2023 20:19
+//*LICENSE:         Academic Free License
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <Arduino.h>
+#include "home.h"
+#ifndef LRES
+#define LRES 16
+#endif
+
+LEDDriver::LEDDriver(pin_t signalPin):_signalPin(signalPin), brightness(0)
+{
+
+}
+
+bool LEDDriver::begin()
+{
+    pinMode(_signalPin, PWM);
+    analogWriteResolution(LRES);
+    return true;
+}
+
+
+pin_t LEDDriver::setBrightness(pin_t brightness)
+{
+    _brightness = brightness;
+    return (getBrightness());
+}
+
+pin_t LEDDriver::getBrightness() const
+{
+    return (_brightness);
+}
+
+pin_t LEDDriver::on()
+{
+    analogWriteResolution(LRES);
+    analogWrite(_signalPin, _brightness);
+    return (getBrightness());
+}
+
+pin_t LEDDriver::off()
+{
+    analogWrite(_signalPin, 0);
+    return (getBrightness());
+}
+
+
