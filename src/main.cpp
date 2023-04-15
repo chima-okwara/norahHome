be18ed6e66ff2c3825e6eaf0077beece3db7edca
bigfavour-2022 <norahlinkstech@gmail.com>
2023-04-15 12:36:36 +0100

    Added Libraries for LCD and servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//*FILE NAME:       main.cpp
-//*FILE DESC:       Main file for norahHome.
-//*FILE VERSION:    0.1.1
-//*FILE AUTHOR:     Favour Aina
-//*LAST MODIFIED:   Saturday, 1 April, 2023 20:19
-//*LICENSE:         Academic Free License
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 #include <Arduino.h>
+#include "home.h"
+#include <LiquidCrystal_I2C.h>
+#include <Servo.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  
+ 
 }
+ 
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+
 }
